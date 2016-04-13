f3c6c275fe4ff4889f655b49e3c3a56545360d70
Kayila <arekuanubis@gmail.com>
2016-04-13 02:04:48 -0400

    Initial commit for 1.8.9 branch

---
+++ package.php
@@ -0,0 +1,11 @@
+<?php
+
+$packages[] = array(
+    'name' => 'Elisium18',
+    'title' => 'Elisium18',
+    'version' => 'VERSION',
+    'priority' => 0,
+    'location' => 'modpacks/elisium18.json',
+);
+
+?>
